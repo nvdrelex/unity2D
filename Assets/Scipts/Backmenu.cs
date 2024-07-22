ce80b0f8f2719fe78a410d3e29e5132bf614f4b8
Do_Monster <hsalldt@gmail.com>
2024-07-22 23:24:35 +0700

    project Fox go home

---
+++ Assets/Scipts/Backmenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Backmenu : MonoBehaviour
+{
+    public void BackMenu()
+    {
+        SceneManager.LoadScene(0); // Thay "GameScene" bằng tên Scene của game bạn.
+    }
+}
---
+++ Assets/Scipts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); // Thay "GameScene" bằng tên Scene của game bạn.
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        // Mở menu tùy chọn (nếu có)
+        Debug.Log("Options");
+    }
+}
